004181f9b738e8afba8141697672aa6899bd6f20
Rahul Hegde <rahulh.hegde@gmail.com>
2021-12-19 13:00:44 -0800

    removed and replaced sensitive info

--- beginnerREST/db.js
+++
@@ -1,2 +0,0 @@
-var mongoose = require("mongoose");//creates an instance of the mongoose class and returns it
-mongoose.connect("mongodb+srv://FirstUser:magic2012@todoapi-qkokp.azure.mongodb.net/test?retryWrites=true&w=majority");
